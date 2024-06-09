73986d5a9fe848d8750216fb53aebffd5428699b
bacravaj <126248255+bacravaj@users.noreply.github.com>
2024-06-09 05:48:47 +0300

    Add files via upload

---
+++ release/CMakeFiles/Chat.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/cpp/final/CPLUS-UI-ChatCPPTest-main/sources/CommandLineInterface.cpp" "CMakeFiles/Chat.dir/sources/CommandLineInterface.cpp.obj" "gcc" "CMakeFiles/Chat.dir/sources/CommandLineInterface.cpp.obj.d"
+  "D:/cpp/final/CPLUS-UI-ChatCPPTest-main/sources/Database.cpp" "CMakeFiles/Chat.dir/sources/Database.cpp.obj" "gcc" "CMakeFiles/Chat.dir/sources/Database.cpp.obj.d"
+  "D:/cpp/final/CPLUS-UI-ChatCPPTest-main/sources/Message.cpp" "CMakeFiles/Chat.dir/sources/Message.cpp.obj" "gcc" "CMakeFiles/Chat.dir/sources/Message.cpp.obj.d"
+  "D:/cpp/final/CPLUS-UI-ChatCPPTest-main/sources/Parsing.cpp" "CMakeFiles/Chat.dir/sources/Parsing.cpp.obj" "gcc" "CMakeFiles/Chat.dir/sources/Parsing.cpp.obj.d"
+  "D:/cpp/final/CPLUS-UI-ChatCPPTest-main/sources/Test.cpp" "CMakeFiles/Chat.dir/sources/Test.cpp.obj" "gcc" "CMakeFiles/Chat.dir/sources/Test.cpp.obj.d"
+  "D:/cpp/final/CPLUS-UI-ChatCPPTest-main/sources/User.cpp" "CMakeFiles/Chat.dir/sources/User.cpp.obj" "gcc" "CMakeFiles/Chat.dir/sources/User.cpp.obj.d"
+  "D:/cpp/final/CPLUS-UI-ChatCPPTest-main/sources/sha1.cpp" "CMakeFiles/Chat.dir/sources/sha1.cpp.obj" "gcc" "CMakeFiles/Chat.dir/sources/sha1.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ release/CMakeFiles/Chat.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Chat.dir/sources/CommandLineInterface.cpp.obj"
+  "CMakeFiles/Chat.dir/sources/CommandLineInterface.cpp.obj.d"
+  "CMakeFiles/Chat.dir/sources/Database.cpp.obj"
+  "CMakeFiles/Chat.dir/sources/Database.cpp.obj.d"
+  "CMakeFiles/Chat.dir/sources/Message.cpp.obj"
+  "CMakeFiles/Chat.dir/sources/Message.cpp.obj.d"
+  "CMakeFiles/Chat.dir/sources/Parsing.cpp.obj"
+  "CMakeFiles/Chat.dir/sources/Parsing.cpp.obj.d"
+  "CMakeFiles/Chat.dir/sources/Test.cpp.obj"
+  "CMakeFiles/Chat.dir/sources/Test.cpp.obj.d"
+  "CMakeFiles/Chat.dir/sources/User.cpp.obj"
+  "CMakeFiles/Chat.dir/sources/User.cpp.obj.d"
+  "CMakeFiles/Chat.dir/sources/sha1.cpp.obj"
+  "CMakeFiles/Chat.dir/sources/sha1.cpp.obj.d"
+  "Chat.exe"
+  "Chat.exe.manifest"
+  "Chat.pdb"
+  "libChat.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Chat.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
